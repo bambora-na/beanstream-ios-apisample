22ce6391601f47e01ababfc55d9c3b9490937da4
Sven Resch <sven.resch@beanstream.com>
2016-01-19 17:50:09 -0800

    MDI-423 Minor L&F change to get status bar to appear with white colour on iOS 9.

--- GoldenEggs/AppDelegate.swift
+++ GoldenEggs/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         UIView.appearance().tintColor = Constants.Colors.mainColor
+        UINavigationBar.appearance().barTintColor = Constants.Colors.secondaryColor
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        UINavigationBar.appearance().barStyle = .Black
         
         // idk: Force update the selection related L&F of every item
         let tabBarController = self.window!.rootViewController as! UITabBarController
